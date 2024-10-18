ba82fcc6b84b8f4ca2dd6986cf183a19338aae7f
Maaxboon <maaxwelokoth@gmail.com>
2024-10-18 11:59:05 +0300

    Prints Hello World

---
+++ MyFirstProject/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[])
+        {
+            Console.Writeline("Hello World!")
+        }
+    }
+}

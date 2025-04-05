544a9d550e72535b1cda334dd236a67b073f56af
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-05 19:30:17 +0300

    Makes fie to accept all cases on prompt inputs

--- 12-class/class-weekend-boring/js/main.js
+++ 12-class/class-weekend-boring/js/main.js
@@ -3,10 +3,10 @@ document.querySelector("#check").addEventListener("click", check);
 function check() {
   const day = document.querySelector("#day").value;
 
-  //Conditionals go here
-  if (day === "Tuesday" || day === "Thursday") {
+  //case sensitive comparisons
+  if (day === "tuesday" || day === "thursday") {
     alert("Class");
-  } else if (day === "Saturday" || day === "Sunday") {
+  } else if (day === "saturday" || day === "sunday") {
     alert("Weekend");
   } else {
     alert("Boring Day");

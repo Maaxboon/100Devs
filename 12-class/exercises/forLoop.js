9b07acc3905cc9613190bfd226c28e7383475f08
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-06 10:45:01 +0300

    Adds for loop

---
+++ 12-class/exercises/forLoop.js
@@ -0,0 +1,10 @@
+/*How it works
+for (initialization; condition; final expression) {
+code to run when the consition is true
+}
+*/
+
+let number;
+for (number = 1; number <= 5; number++) {
+  console.log(number);
+}

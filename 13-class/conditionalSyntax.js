7c8fcd0099d92c1244fc55fe054d4e7413155ce2
Maaxboon <maaxwelokoth@gmail.com>
2025-05-08 13:10:34 +0300

    Adds conditional syntax

---
+++ 13-class/conditionalSyntax.js
@@ -0,0 +1,9 @@
+bestTeam = "Manchester United";
+
+if (bestTeam === "Barcelona") {
+  console.log("Almost there");
+} else if (bestTeam === "Manchester United") {
+  console.log("You know ball");
+} else {
+  console.log("Find another sport mate!");
+}
